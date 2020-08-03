b79c191b0358181b3a2bd7488597ab4f30913694
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-03 22:17:31 +0200

    making homePage component

---
+++ src/homepage.component.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+const HomePage = () => (
+    <div className='homepage'>
+            <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>hats</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Jackets</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Sneakers</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>womens</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>mens</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+);
+export default HomePage;
