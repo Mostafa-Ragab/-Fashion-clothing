0a5d2bd36b1a7aa9ee98e107536370dc786ccc79
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-03 22:32:47 +0200

    adding styling to home page

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,38 +1,43 @@
 import React from 'react';
+
+import './homepage.styles.scss';
+
 const HomePage = () => (
-    <div className='homepage'>
-            <div className='directory-menu'>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>hats</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>Jackets</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>Sneakers</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>womens</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>mens</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
+  <div className='homepage'>
+    <h1>Welcome to my Homepage</h1>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>HATS</div>
+          <span className='subtitle'>SHOP NOW</span>
         </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>JACKETS</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>SHOES</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>WOMENS</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>MENS</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
 );
+
 export default HomePage;
