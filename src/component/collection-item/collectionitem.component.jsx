7aca9d9e87f52d5bdb33080b7fd46c46b273c94f
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-04 16:31:34 +0200

    fixing bugs in src/component/collection-item/collectionitem.component.jsx

--- src/component/collection-item/collectionitem.component.jsx
+++ src/component/collection-item/collectionitem.component.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import './collectionitem.styles.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <div className='collection-item'>
-      <div
-        className='image'
-        style={{
-          backgroundImage: `url(${imageUrl})`
-        }}
-      />
-      <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
-      </div>
+  <div className='collection-item'>
+    <div
+      className='image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='collection-footer'>
+      <span className='name'>{name}</span>
+      <span className='price'>{price}</span>
     </div>
-  );
-  
-  export default CollectionItem;
+  </div>
+);
+
+export default CollectionItem;
