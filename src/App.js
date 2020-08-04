c429692c45071ea26da6dde33b405b8f95c7b64f
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-04 16:30:50 +0200

    adding switch and header to app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage.component'
 import './App.css';
 import ShopPage from './pages/shop/shop.component';
+import Header from './component/header/header.component';
 
 
 
 function App() {
   return (
     <div >
+    <Header  />
     <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route exact path='/shop' component={ShopPage}/>
