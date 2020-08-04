3645485cf622ab6f3510a7d901bbfa8f0884f792
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-04 14:00:18 +0200

    making route shop

--- src/App.js
+++ src/App.js
@@ -5,17 +5,13 @@ import './App.css';
 import ShopPage from './pages/shop/shop.component';
 
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+
 function App() {
   return (
     <div >
     <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route exact path='/' component={ShopPage}/>
+      <Route exact path='/shop' component={ShopPage}/>
     </Switch>
     </div>
   );
