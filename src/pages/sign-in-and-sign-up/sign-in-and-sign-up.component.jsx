fe0fccdf1848af57eda62e14bccd13a1d7ff5b62
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-04 18:41:09 +0200

    making sign-in and saign up page

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './sign-in-and-sing-up.styles.scss';
+import SignIn from '../../component/signin/signin.component';
+const SignInAndSignUpPage = () => (
+    <div className='sign-in-and-sing-up'>
+        <SignIn  />
+    </div>
+);
+
+export default SignInAndSignUpPage;
