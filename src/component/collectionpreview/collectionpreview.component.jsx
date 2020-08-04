5ba5b7662978058eb702d7d4223352951404bb75
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-04 16:33:29 +0200

    changing our Rout

--- src/component/collectionpreview/collectionpreview.component.jsx
+++ src/component/collectionpreview/collectionpreview.component.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
-import './collectionpreview.component.styles.scss';
 import CollectionItem from '../collection-item/collectionitem.component';
 
+import './collection-preview.styles.scss';
+
+
 const CollectionPreview = ({ title, items }) => (
-    <div className='collection-preview'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <div className='preview'>
-        {items
-          .filter((item, idx) => idx < 4)
-          .map(({ id, ...otherItemProps }) => (
-            <CollectionItem key={id} {...otherItemProps} />
-          ))}
-      </div>
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
     </div>
-  );
-  
-  export default CollectionPreview;
+  </div>
+);
+
+export default CollectionPreview;
--- src/component/menu-item/menu-item.component.jsx
+++ src/component/menu-item/menu-item.component.jsx
@@ -4,6 +4,7 @@ import {withRouter} from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
+
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div
     className={`${size} menu-item`}
