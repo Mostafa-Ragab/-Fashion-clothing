c5676576ec60ef76e237c3d17580fdc779bd6039
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-04 13:58:01 +0200

    adding shop page component to app.js and shop data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage.component'
 import './App.css';
+import ShopPage from './pages/shop/shop.component';
 
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 function App() {
   return (
     <div >
-      <HomePage   />
+    <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/' component={ShopPage}/>
+    </Switch>
     </div>
   );
 }
