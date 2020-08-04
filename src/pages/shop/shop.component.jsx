78a853388d317c1c08a945d1df8b8a7fd4685c12
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-04 14:35:48 +0200

    fixing bugs in collectionPreview

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../component/collectionpreview/collectionpreview.component';
+
 class ShopPage extends React.Component {
-    constructor() {
-        super();
-        this.state= SHOP_DATA
+    constructor(props) {
+        super(props);
+        this.state= {
+            collections: SHOP_DATA
+        };
+        
     }
     render() {
+        const {collections}=this.state;
         return (
-            <div>shop page</div>
+            <div className='shop-page'>
+                {collections.map(({id, ...otherCollectionProps}) => (
+                    <CollectionPreview key={id} {...otherCollectionProps}/>
+                    ))
+                }
+            </div>
         )
     }
 
