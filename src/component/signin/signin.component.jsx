4e1a334e2ad1a21cfb1638f4f313a3539e28030f
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-05 05:41:36 +0200

    adding firebase sign in adn sign out

--- src/component/signin/signin.component.jsx
+++ src/component/signin/signin.component.jsx
@@ -1,59 +1,66 @@
 import React from 'react';
+
 import FormInput from '../form-input/form-input.component';
-import './signin.styles.scss';
+
+import './signin.styles.scss'
+import { signInWithGoogle } from '../firebase/firebase.utiles';
 import CustomButton from '../custom-button/custom-button.componenet';
-import signInWithGoogle from '../firebase/firebase.utiles'
+
 class SignIn extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
-    handleChange = event => {
-        const { value, name } = event.target;
-    
-        this.setState({ [name]: value });
+      super(props);
+  
+      this.state = {
+        email: '',
+        password: ''
       };
-    handleSubmit = event => {
-        event.preventDefault();
-        this.setstate({name:'' , password:'' })
-    };
-
-    render() {
-        return(
-            <div className='sign-in'>
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
-                
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput
-                        handleChange={this.handleChange} 
-                        name='email' 
-                        type='email' 
-                        value={this.state.email} 
-                        required 
-                        label='email'
-
-                    />
-                    <FormInput 
-                        handleChange={this.handleChange} 
-                        name='password' 
-                        type='password' 
-                        value={this.state.password} 
-                        required 
-                        label='password'
-
-                    />
-                    <CustomButton type='submit'  > Sign in</CustomButton>
-                    <CustomButton onclick={signInWithGoogle}  >
-                    {' '} 
-                    sign in with google{' '}
-                    </CustomButton>
-                </form>
-            </div>
-        )
     }
-}
+  
+    handleSubmit = event => {
+      event.preventDefault();
+  
+      this.setState({ email: '', password: '' });
+    };
+  
+    handleChange = event => {
+      const { value, name } = event.target;
+  
+      this.setState({ [name]: value });
+    };
+  
+    render() {
+      return (
+        <div className='sign-in'>
+          <h2>I already have an account</h2>
+          <span>Sign in with your email and password</span>
+  
+          <form onSubmit={this.handleSubmit}>
+            <FormInput
+              name='email'
+              type='email'
+              handleChange={this.handleChange}
+              value={this.state.email}
+              label='email'
+              required
+            />
+            <FormInput
+              name='password'
+              type='password'
+              value={this.state.password}
+              handleChange={this.handleChange}
+              label='password'
+              required
+            />
+            <div className='buttons'>
+              <CustomButton type='submit'> Sign in </CustomButton>
+              <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                Sign in with Google
+              </CustomButton>
+            </div>
+          </form>
+        </div>
+      );
+    }
+  }
+  
 export default SignIn;
