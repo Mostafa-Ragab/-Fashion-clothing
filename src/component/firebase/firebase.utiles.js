1d3a6a9e90bd926ff0eb93e4fba12bb0bcac390f
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-05 05:42:01 +0200

    adding styles to buttons

--- src/component/firebase/firebase.utiles.js
+++ src/component/firebase/firebase.utiles.js
@@ -2,23 +2,24 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-    apiKey: "AIzaSyC1nvpZdOIA5tNzW_4lZjNvG97Z6nidJOs",
-    authDomain: "crwn-db-270f9.firebaseapp.com",
-    databaseURL: "https://crwn-db-270f9.firebaseio.com",
-    projectId: "crwn-db-270f9",
-    storageBucket: "crwn-db-270f9.appspot.com",
-    messagingSenderId: "830172283755",
-    appId: "1:830172283755:web:5deac256869a726aebc22e",
-    measurementId: "G-57Q1TKMMQZ"
-  };
-  firebase.initializeApp(config);
+const firebaseConfig = {
+  apiKey: "AIzaSyC1nvpZdOIA5tNzW_4lZjNvG97Z6nidJOs",
+  authDomain: "crwn-db-270f9.firebaseapp.com",
+  databaseURL: "https://crwn-db-270f9.firebaseio.com",
+  projectId: "crwn-db-270f9",
+  storageBucket: "crwn-db-270f9.appspot.com",
+  messagingSenderId: "830172283755",
+  appId: "1:830172283755:web:ff0c137324babbe5ebc22e",
+  measurementId: "G-EQVPECJBXV"
+};
+  
+firebase.initializeApp(firebaseConfig);
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
-
+  
   const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({prompt: 'select_account'});
-  export const signInWithGoogle = () =>auth.signInWithPopup(provider);
-
+  provider.setCustomParameters({ prompt: 'select_account' });
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  
   export default firebase;
