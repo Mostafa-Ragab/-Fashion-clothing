daeb7b81fa18d6928dd2c9688e90095d14e89ea4
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-05 05:40:05 +0200

    importing auth from firebase and added to header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
+
 import {Switch, Route} from 'react-router-dom';
+
 import HomePage from './pages/homepage.component'
+
 import './App.css';
+
 import ShopPage from './pages/shop/shop.component';
+
 import Header from './component/header/header.component';
+
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
+import {auth} from './component/firebase/firebase.utiles'
 
 
-function App() {
-  return (
-    <div >
-    <Header  />
-    <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route exact path='/shop' component={ShopPage}/>
-      <Route exact path='/signin' component={SignInAndSignUpPage}/>
-    </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
-
 export default App;
