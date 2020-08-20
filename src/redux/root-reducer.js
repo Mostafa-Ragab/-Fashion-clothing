598ea97e6e758a5cadf8ddaee417d466156a5537
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-20 04:10:24 +0200

    adding directory state inside redux to make it reusable

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -7,6 +7,7 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user-reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 
 const persistConfig = {
   key: 'root',
@@ -17,7 +18,9 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  dicrectory: directoryReducer
+
 });
 
 
