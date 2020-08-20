fa631ca1ec5188ff40aeed707ed14a531464c3f3
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-20 07:16:43 +0200

    added categorypage

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './category.styles.scss';
+import collectionItem from '../../component/collection-item/collectionitem.component';
+const CategoryPage = () => (
+    <div className='category'>
+        <h2>category page</h2>
+
+    </div>
+);
+
+
+export default CategoryPage;
