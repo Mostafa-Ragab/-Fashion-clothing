e28f2dd254302760a2a40bae028a1ec32d42ef94
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-20 09:37:57 +0200

    added selec collection map to shop selectors

--- src/pages/category/category.component.jsx
+++ src/pages/category/category.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './category.styles.scss';
 import collectionItem from '../../component/collection-item/collectionitem.component';
-const CategoryPage = () => (
+import { selectCollections } from '../../redux/shop/shop.selector';
+import { connect } from 'react-redux';
+const CategoryPage = ({collection}) => (
     <div className='category'>
         <h2>category page</h2>
 
@@ -9,4 +11,8 @@ const CategoryPage = () => (
 );
 
 
-export default CategoryPage;
+const mapStateToProps = (state, ownprops) => ({
+    collection: selectCollections(ownprops.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CategoryPage);
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,5 +1,12 @@
 import {createSelector} from 'reselect';
 
+const COLLECTIONS_ID_MAP = {
+    hats: 1,
+    sneakers:2,
+    jackets:3,
+    womens: 4,
+    mens: 4
+};
 
 const selectShop = state => state.shop;
 
@@ -7,3 +14,10 @@ export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 )
+
+export const selectCollection = collectionUrlParam => createSelector(
+    [COLLECTIONS_ID_MAP],
+    collections => collections.find(
+        collection => collection.id ===COLLECTIONS_ID_MAP(collectionUrlParam)
+    )
+);
