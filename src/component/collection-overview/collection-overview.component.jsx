d9747a17739712543b8df78d77d1de6c0b40cf83
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-20 06:27:08 +0200

    dixing name of shop collections selectors

--- src/component/collection-overview/collection-overview.component.jsx
+++ src/component/collection-overview/collection-overview.component.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import './collection-overview.styles.scss';
 import { connect } from 'react-redux';
 import {createStructuredSelector} from 'reselect';
-import {selectShopItems} from '../../redux/shop/shop.selector';
 import CollectionPreview from '../collectionpreview/collectionpreview.component';
-
+import {selectCollections} from '../../redux/shop/shop.selector';
 
 const CollectionsOverview = ({ collections }) => (
     <div className='collections-overview'>
@@ -15,7 +14,7 @@ const CollectionsOverview = ({ collections }) => (
   );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectShopItems
+    collections: selectCollections
   });
   
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -3,7 +3,7 @@ import {createSelector} from 'reselect';
 
 const selectShop = state => state.shop;
 
-export const selectShopItems = createSelector(
+export const selectCollections = createSelector(
     [selectShop],
-    shop => shop.shopItems
+    shop => shop.collections
 )
