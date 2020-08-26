716d5eb86744102b2a81b57f4b76dcb6bd4d9514
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-26 13:09:03 +0200

    added shop actions start/success/failure

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,26 @@
 import shopActionTypes from './shop.types';
-
-export const updateCollections = (collectionsMap) => ({
-    type: shopActionTypes.UPDATE_COLLECTIONS,
-    payload: collectionsMap
+import {firestore, convertCollcetionsSnapshotToMap} from '../../component/firebase/firebase.utiles';
+export const fetchCollectionsStart = () => ({
+    type: shopActionTypes.FETCH_COLLECTIONS_START
 });
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+    type: shopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+    payload: collectionsMap
+}) 
+
+export const fetchCollectionsFailure = errorMessage => ({
+    type: shopActionTypes.fetchCollectionsFailure,
+    payload: errorMessage
+})
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections');
+        dispatch(fetchCollectionsStart());
+
+        collectionRef.get().then(snapshot => {
+          const collectionsMap = convertCollcetionsSnapshotToMap(snapshot)
+          dispatch(fetchCollectionsSuccess(collectionsMap));
+        }).catch(error => dispatch(fetchCollectionsFailure(error.message)))
+    }
+}
