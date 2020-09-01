c23ce445f718884e8e64af9df52014414e887f08
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-09-01 16:19:30 +0200

    added redux- thunk into saga

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,31 @@
+import {takeEvery, call, put} from 'redux-saga/effects';
+
+import ShopActionTypes from './shop.types';
+import {firestore, convertCollectionsSnapshotToMap } from '../../component/firebase/firebase.utiles';
+import { 
+    fetchCollectionsFailure,
+fetchCollectionsSuccess 
+} from './shop.actions';
+
+export function* fetchCollectionAsync () {
+    try {
+
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(
+            convertCollectionsSnapshotToMap,
+            snapshot
+        );  
+        yield put(fetchCollectionsSuccess(collectionsMap));
+    } catch (error) {
+        yield put(fetchCollectionsFailure(error.message))
+    }    
+}
+
+
+
+export function* fetchCollectionStart() {
+    yield takeEvery(
+        ShopActionTypes.FETCH_COLLECTIONS_START,
+        fetchCollectionAsync )
+}
