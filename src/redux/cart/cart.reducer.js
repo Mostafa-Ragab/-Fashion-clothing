83f4adaff3c2b06dc49016da876095bc65277400
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-09-02 19:04:34 +0200

    added clear cart case inside reducer cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -30,6 +30,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         cartItems: RemoveItemFromCart(state.cartItems, action.payload)
       };
+    case CartActionTypes.CLEAR_CART:
+      return {
+        ...state,
+        cartItems:[]
+      }
 
     default:
       return state; 
