3e76e3da8f6d922fb74616962c65387adff323ba
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-09-02 16:52:45 +0200

    added user saga into root saga

--- src/redux/root.sagas.js
+++ src/redux/root.sagas.js
@@ -1,9 +1,9 @@
 import {all, call } from 'redux-saga/effects';
 import {fetchCollectionStart } from './shop/shop.sagas';
-
+import {userSagas } from './user/user.sagas';
 
 export default function* rootSaga() {
     all([
-      yield call(fetchCollectionStart)
+      yield all([call(fetchCollectionStart), call(userSagas)]) 
     ])
 };
