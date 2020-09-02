e8b69241c94042b361ed3bed271cae0b83147ef8
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-09-02 18:01:59 +0200

    added sign out actions inside saga

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -1,8 +1,7 @@
 import {takeLatest, put, all, call } from 'redux-saga/effects';
 import UserActionTypes from './user.types';
 import {auth , googleProvider, createUserProfileDocument,getCurrentUser  } from '../../component/firebase/firebase.utiles'
-import { SignInSuccess, SignInFailure } from './user.action';
-
+import { SignInSuccess, SignInFailure,signOutSuccess,signOutFailure } from './user.action';
 export function* getSnapshotFromUserAuth(userAuth) {
     try {
         const userRef = yield call(createUserProfileDocument, userAuth);
@@ -44,7 +43,13 @@ export function* onEmailSignIn() {
     yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START,signInWithEmail)
 }
 export function* userSagas() {
-    yield all([call(onGoogleSignIn), call(onEmailSignIn), call(isUserAuthenticated)])
+    yield all(
+        [
+            call(onGoogleSignIn),
+            call(onEmailSignIn), 
+            call(isUserAuthenticated), 
+            call(signOutStart)
+        ])
 }
 
 export function* isUserAuthenticated() {
@@ -60,4 +65,16 @@ export function* isUserAuthenticated() {
 };
 export function* OnCheackUserSeasion() {
     yield takeLatest(UserActionTypes.CHECK_USER_SEASION, isUserAuthenticated)
-} 
+}
+export function* signOut() {
+    try {
+        yield auth.signOut();
+        yield put(signOutSuccess());
+    } catch (error) {
+        yield put(signOutFailure(error))
+    }
+}
+
+export function* signOutStart() {
+    yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut)
+}
