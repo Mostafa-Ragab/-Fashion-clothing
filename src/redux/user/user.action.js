7f59502c32c5ad59de9b05a86d9aab9f7565dd82
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-09-02 16:53:37 +0200

    added user actions

--- src/redux/user/user.action.js
+++ src/redux/user/user.action.js
@@ -1,6 +1,26 @@
-import { UserActionTypes } from './user.types';
+import  UserActionTypes  from './user.types';
 
-export const setCurrentUser = user => ({
-  type: UserActionTypes.SET_CURRENT_USER,
+export const googleSignInStart = () => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_START
+});
+
+export const SignInSuccess = user => ({
+  type: UserActionTypes.SIGN_IN_SUCCESS,
   payload: user
 });
+
+export const SignInFailure = error => ({
+  type: UserActionTypes.SIGN_IN_FAILURE,
+  payload:error
+});
+export const emailSignInStart = emailAndPassword=> ({
+  type: UserActionTypes.EMAIL_SIGN_IN_START,
+  payload: emailAndPassword
+});
+
+export const cheackUserSeasion = () => ({
+  type: UserActionTypes.CHECK_USER_SEASION
+});
+
+
+
