a4b1b13c5a4104a5466f357ea5f30f834d0609e7
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-09-02 16:51:04 +0200

    added user saga

---
+++ src/redux/user/user.sagas.js
@@ -0,0 +1,63 @@
+import {takeLatest, put, all, call } from 'redux-saga/effects';
+import UserActionTypes from './user.types';
+import {auth , googleProvider, createUserProfileDocument,getCurrentUser  } from '../../component/firebase/firebase.utiles'
+import { SignInSuccess, SignInFailure } from './user.action';
+
+export function* getSnapshotFromUserAuth(userAuth) {
+    try {
+        const userRef = yield call(createUserProfileDocument, userAuth);
+        const userSnapShot = yield userRef.get();
+        yield put (
+            SignInSuccess({id: userSnapShot.id, ...userSnapShot.data()})
+        )
+    } catch (error) {
+        yield put (
+            SignInFailure(error)
+        )
+    }
+}
+export function* signInWithGoogle() {
+    try {
+        const {user} = yield auth.signInWithPopup(googleProvider);
+        yield getSnapshotFromUserAuth(user)
+    } catch (error) {
+        yield put (
+            SignInFailure(error)
+        )
+    }
+}
+export function* onGoogleSignIn() {
+    yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle)
+}
+
+export function* signInWithEmail({payload:{email,password}}) {
+    try {
+        const {user} = yield auth.signInWithEmailAndPassword(email, password);
+        yield getSnapshotFromUserAuth(user)
+    } catch (error) {
+        yield put (
+            SignInFailure(error)
+        )
+    }
+}
+export function* onEmailSignIn() {
+    yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START,signInWithEmail)
+}
+export function* userSagas() {
+    yield all([call(onGoogleSignIn), call(onEmailSignIn), call(isUserAuthenticated)])
+}
+
+export function* isUserAuthenticated() {
+    try {
+        const userAuth = yield getCurrentUser();
+        if(!userAuth) return ;
+        yield getSnapshotFromUserAuth(userAuth);
+    } catch (error) {
+        yield put (
+            SignInFailure(error)
+        )
+    }
+};
+export function* OnCheackUserSeasion() {
+    yield takeLatest(UserActionTypes.CHECK_USER_SEASION, isUserAuthenticated)
+} 
