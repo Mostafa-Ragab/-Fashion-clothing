1044fb3d2330b3e88a6a83be81a8c2573861672e
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-09-02 19:05:27 +0200

    added cat sagas inside root saga

--- src/redux/root.sagas.js
+++ src/redux/root.sagas.js
@@ -1,9 +1,9 @@
 import {all, call } from 'redux-saga/effects';
 import {fetchCollectionStart } from './shop/shop.sagas';
 import {userSagas } from './user/user.sagas';
-
+import {cartSaga} from './cart/cart.sagas';
 export default function* rootSaga() {
     all([
-      yield all([call(fetchCollectionStart), call(userSagas)]) 
+      yield all([call(fetchCollectionStart), call(userSagas), call(cartSaga)]) 
     ])
 };
