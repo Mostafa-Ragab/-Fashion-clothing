9011c7fdfd319d761b2d124adfee55434dd613b4
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-09-03 03:38:44 +0200

    added shop sagas and user sagas into root saga

--- src/redux/root.sagas.js
+++ src/redux/root.sagas.js
@@ -1,9 +1,10 @@
 import {all, call } from 'redux-saga/effects';
-import {fetchCollectionStart } from './shop/shop.sagas';
+import {shopSagas } from './shop/shop.sagas';
 import {userSagas } from './user/user.sagas';
 import {cartSaga} from './cart/cart.sagas';
+
 export default function* rootSaga() {
     all([
-      yield all([call(fetchCollectionStart), call(userSagas), call(cartSaga)]) 
+      yield all([call(shopSagas), call(userSagas), call(cartSaga)]) 
     ])
 };
